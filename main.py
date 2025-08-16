319b2cc2e0caa43d299f71cf188910d49a155cdf
xAKI85x <AlexanderKromm85@web.de>
2025-08-16 07:25:50 +0200

    Projekt Fertiggestellt

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from stats import get_book_text, get_wordcount, convert
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main ():
-    get_book_text("books/frankenstein.txt")
-    char_count = get_wordcount("books/frankenstein.txt")
+    get_book_text(sys.argv[1])
+    char_count = get_wordcount(sys.argv[1])
     gezählt = convert(char_count)
     for g in gezählt:
         print(f"{g["char"]}: {g["num"]}")
